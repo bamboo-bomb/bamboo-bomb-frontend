d25516c4fb452a0ad1f3345d71e27f3b203d6323
ryukyunghye <ryukyung123@naver.com>
2025-01-16 15:35:09 +0900

    message(common/#3): 필요없는 태그 삭제 및 import 위치 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,6 @@
-import SelectBox from './SelectBox';
 import { useState } from 'react';
 import Button from '../Button';
+import SelectBox from './SelectBox';
 
 interface HeaderProps {
   title: '대나무' | '대나무숲' | '해우소' | '💣';
@@ -22,36 +22,30 @@ export default function Header(props: HeaderProps) {
   };
 
   return (
-    <>
-      <header className="w-full p-[1.2rem] flex justify-between items-center bg-green-3 text-green-1 text-1 font-regular">
-        <div className="w-[5.3rem] flex items-center">
-          {isBoard || (
-            <Button
-              type="header"
-              iconName="arrow"
-              onClick={onClickArrowButton}
-            />
-          )}
-        </div>
-        <h1>{title}</h1>
-        <div className="w-[5.3rem] flex justify-end items-center relative">
-          {isOwner && (
-            <>
-              <Button type="header" iconName="dot" onClick={onClickDot3} />
-              {isDropdownOpen && (
-                <div className="absolute top-full right-0 mt-[0.4rem] w-[7.4rem]">
-                  <Button
-                    type="delete"
-                    iconName="trash"
-                    onClick={onClickDelete}
-                  />
-                </div>
-              )}
-            </>
-          )}
-          {isBoard && <SelectBox />}
-        </div>
-      </header>
-    </>
+    <header className="w-full p-[1.2rem] flex justify-between items-center bg-green-3 text-green-1 text-1 font-regular">
+      <div className="w-[5.3rem] flex items-center">
+        {isBoard || (
+          <Button type="header" iconName="arrow" onClick={onClickArrowButton} />
+        )}
+      </div>
+      <h1>{title}</h1>
+      <div className="w-[5.3rem] flex justify-end items-center relative">
+        {isOwner && (
+          <>
+            <Button type="header" iconName="dot" onClick={onClickDot3} />
+            {isDropdownOpen && (
+              <div className="absolute top-full right-0 mt-[0.4rem] w-[7.4rem]">
+                <Button
+                  type="delete"
+                  iconName="trash"
+                  onClick={onClickDelete}
+                />
+              </div>
+            )}
+          </>
+        )}
+        {isBoard && <SelectBox />}
+      </div>
+    </header>
   );
 }
