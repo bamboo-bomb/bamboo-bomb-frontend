c9dca0bb70b2c11237a69067ab768d1e11a37d77
ryukyunghye <ryukyung123@naver.com>
2025-01-16 15:12:40 +0900

    design(common/#3): 페이지마다 layout, header 적용

--- src/pages/BoardDetail.tsx
+++ src/pages/BoardDetail.tsx
@@ -1,10 +1,11 @@
 import { useParams } from 'react-router-dom';
+import Layout from '../components/common/Layout';
 
 export default function BoardDetail() {
   const { id } = useParams();
   return (
-    <>
+    <Layout>
       <h1>게시글:{id}</h1>
-    </>
+    </Layout>
   );
 }
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,9 @@
+import Layout from '../components/common/Layout';
+
 export default function MyPage() {
   return (
-    <>
+    <Layout>
       <h1>마이 페이지</h1>
-    </>
+    </Layout>
   );
 }
