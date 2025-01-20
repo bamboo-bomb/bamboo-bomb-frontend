753dd0223ae4e56de8b62eeabf229c38231c9765
ryukyunghye <ryukyung123@naver.com>
2025-01-21 04:19:20 +0900

    feat(common/#9): 정렬 기준 쿼리 스트링에 담기 및 뒤로가기 구현

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import Button from '../Button';
 import SelectBox from './SelectBox';
 
@@ -12,31 +13,39 @@ export default function Header(props: HeaderProps) {
   const { title, isBoard = false, isOwner = false } = props;
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const onClickArrowButton = () => alert('back arrow');
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onClickArrowButton = () => {
+    if (location.pathname.includes('/bomb')) return navigate(-2);
+    return navigate(-1);
+  };
 
   const onClickDot3 = () => setIsDropdownOpen(true);
 
   const onClickDelete = () => {
     setIsDropdownOpen(false);
     alert('trash');
+    // TODO: 게시글 삭제 요청
+    navigate(-1);
   };
 
   return (
     <header className="w-full p-[1.2rem] flex justify-between items-center bg-green-3 text-green-1 text-1 font-regular">
       <div className="w-[5.3rem] flex items-center">
         {isBoard || (
-          <Button type="header" iconName="arrow" onClick={onClickArrowButton} />
+          <Button kind="header" iconName="arrow" onClick={onClickArrowButton} />
         )}
       </div>
       <h1>{title}</h1>
       <div className="w-[5.3rem] flex justify-end items-center relative">
         {isOwner && (
           <>
-            <Button type="header" iconName="dot" onClick={onClickDot3} />
+            <Button kind="header" iconName="dot" onClick={onClickDot3} />
             {isDropdownOpen && (
               <div className="absolute top-full right-0 mt-[0.4rem] w-[7.4rem]">
                 <Button
-                  type="delete"
+                  kind="delete"
                   iconName="trash"
                   onClick={onClickDelete}
                 />
--- src/components/common/header/SelectBox.tsx
+++ src/components/common/header/SelectBox.tsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function SelectBox() {
+  const [sortBy, setSortBy] = useState('default');
+
+  const navigate = useNavigate();
+
+  const onChangeSelectBox = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const changedSortBy = e.target.value;
+    navigate(`/board?sort=${changedSortBy}`);
+    setSortBy(changedSortBy);
+  };
+
+  // TODO: sortBy 사용해서 데이터 20개씩 호출
+  console.log('sortBy', sortBy);
+
   return (
-    <select name="sort" id="sort" className="bg-inherit font-regular text-3">
+    <select
+      name="sort"
+      id="sort"
+      className="bg-inherit font-regular text-3"
+      onChange={onChangeSelectBox}>
       <option value="default">기본</option>
       <option value="popular">인기순</option>
       <option value="view">조회순</option>
