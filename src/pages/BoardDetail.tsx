ead619914e1aa5a5f822583914ba98a4fbc06a7f
ryukyunghye <ryukyung123@naver.com>
2025-02-04 01:05:42 +0900

    feat(board/#11): 데이터베이스 상태에 맞게 더미 데이터 수정 및 반영

--- src/pages/BoardDetail.tsx
+++ src/pages/BoardDetail.tsx
@@ -2,17 +2,24 @@ import Layout from '../components/common/Layout';
 import UserProfile from '../components/UserProfile';
 import Bulletin from '../components/common/Bulletin';
 import dummy from '../dummy/bulletin.json';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function BoardDetail() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const post = dummy.find((item) => item.id.toString() === id);
+  if (!post) navigate('/bomb');
   return (
     <Layout>
       <UserProfile time={30} />
       <Bulletin
-        title={dummy[1].title}
-        content={dummy[1].content}
-        time={dummy[1].time}
+        title={post.title}
+        content={post.content}
+        time={post.time}
+        reactions={post.reactions}
+        userReaction={post.userReactions?.[post?.authorId] ?? null}
         isBoardDetail
-        isShowZero
       />
     </Layout>
   );
