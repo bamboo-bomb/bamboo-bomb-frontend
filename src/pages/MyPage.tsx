b22022f510f0287406a13325dfa4770c6447857f
ryukyunghye <ryukyung123@naver.com>
2025-02-04 03:54:39 +0900

    message(common/#13): 게시글 리스트 컴포넌트 반영

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,23 +1,12 @@
-import Bulletin from '../components/common/Bulletin';
+import BulletinList from '../components/common/BulletinList';
 import Layout from '../components/common/Layout';
 import UserProfile from '../components/UserProfile';
-import dummy from '../dummy/bulletin.json';
 
 export default function MyPage() {
   return (
     <Layout>
       <UserProfile isMypage />
-      <ul className="overflow-y-auto h-[calc(100%-10.1rem)]">
-        {dummy.map((item) => (
-          <Bulletin
-            key={item.id}
-            title={item.title}
-            content={item.content}
-            time={item.time}
-            isShowTime
-          />
-        ))}
-      </ul>
+      <BulletinList isMyPage />
     </Layout>
   );
 }
