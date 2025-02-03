e8f963f20a9cd2e84f4c84a334511a66f3e93e31
ryukyunghye <ryukyung123@naver.com>
2025-02-04 01:05:04 +0900

    feat(board/#11): 게시글 기능 구현

--- src/components/common/Bulletin.tsx
+++ src/components/common/Bulletin.tsx
@@ -1,28 +1,35 @@
+import { ReactionListTextType } from '../../types/Bulletin';
 import EmojiList from './EmojiList';
+
 interface BulletinProps {
   title: string;
   content: string;
+  time: number | string;
+  reactions: ReactionListTextType;
+  userReaction: null | keyof ReactionListTextType;
   isBoardDetail?: boolean;
   isShowTime?: boolean;
   isShowZero?: boolean;
-  time: number | string;
 }
 
 export default function Bulletin(props: BulletinProps) {
   const {
     title,
     content,
+    time,
+    reactions,
+    userReaction,
     isBoardDetail = false,
     isShowTime = false,
     isShowZero = false,
-    time,
   } = props;
+
   return (
     <div
       className={`w-full px-[1.2rem] py-[.8rem] flex justify-center items-center flex-col ${
         isBoardDetail
           ? 'gap-[1.6rem]'
-          : 'gap-[.8rem] border-b border-b-black-20'
+          : 'gap-[.8rem] border-b border-b-black-20 cursor-pointer'
       }`}>
       <p className="w-full flex justify-between text-green-5">
         <span className="text-2">{title}</span>
@@ -31,7 +38,11 @@ export default function Bulletin(props: BulletinProps) {
       <p className={`w-full text-3 ${isBoardDetail || 'line-clamp-2'}`}>
         {content}
       </p>
-      <EmojiList isShowZero={isShowZero} />
+      <EmojiList
+        isShowZero={isShowZero}
+        reactions={reactions}
+        userReaction={userReaction}
+      />
     </div>
   );
 }
--- src/components/common/EmojiList.tsx
+++ src/components/common/EmojiList.tsx
@@ -1,27 +1,64 @@
+import { useState } from 'react';
+import { ReactionListTextType } from '../../types/Bulletin';
 import Button from './Button';
-
-const emojiList = {
-  '👍': '1',
-  '❤️': '0',
-  '😨': '99+',
-  '😀': '999+',
-  '😡': '99+',
-};
+import {
+  emojiList,
+  formatReactionCount,
+  hiddenReactionZero,
+} from '../../utils/emojiList';
 
 interface EmojiListProps {
   isShowZero?: boolean;
+  reactions: ReactionListTextType;
+  userReaction: string | null;
 }
 
-export default function EmojiList({ isShowZero }: EmojiListProps) {
-  const filteredEmojiList = Object.entries(emojiList).filter(
-    ([_, count]) => isShowZero || count !== '0'
+export default function EmojiList(props: EmojiListProps) {
+  const { isShowZero, reactions, userReaction } = props;
+
+  const [reactionList, setReactionList] = useState(reactions);
+  const [selected, setSelected] = useState<keyof ReactionListTextType | null>(
+    userReaction || null
   );
+
+  const onClickEmojiButton = (
+    name: keyof ReactionListTextType,
+    e: React.MouseEvent
+  ) => {
+    e.stopPropagation();
+    setReactionList((prev) => {
+      const newReactions = { ...prev };
+
+      // 이모지 선택 취소 시 count 감소 & 현재 상태 null
+      if (selected === name) {
+        newReactions[name] = Math.max(0, prev[name] - 1);
+        setSelected(null);
+      }
+      // 이전 선택이 있었다면 이전 count 감소, 현재 선택 count 증가 & 현재 상태 변경
+      else {
+        if (selected) newReactions[selected] = Math.max(0, prev[selected] - 1);
+        newReactions[name] = prev[name] + 1;
+        setSelected(name);
+      }
+      // TODO: DB에 현재 reactions 저장
+      return newReactions;
+    });
+  };
+
+  const filteredReactionList = hiddenReactionZero(isShowZero, reactionList);
 
   return (
     <ul className="w-full flex justify-start gap-[.8rem]">
-      {filteredEmojiList.map(([emoji, count], index) => (
-        <li key={index}>
-          <Button kind="emoji" content={`${emoji} ${count}`} />
+      {Object.entries(filteredReactionList).map(([name, count]) => (
+        <li key={name}>
+          <Button
+            kind="emoji"
+            content={`${emojiList[name]} ${formatReactionCount(count)}`}
+            className={`flex items-center justify-center gap-2 ${
+              selected === name ? 'border-green-4' : 'border-green-1'
+            }`}
+            onClick={(e) => onClickEmojiButton(name as keyof ReactionsType, e)}
+          />
         </li>
       ))}
     </ul>
